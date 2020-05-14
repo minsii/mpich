2360b6e5febefb4b9b163d85819b1b81fa1865d4
Min Si <msi@anl.gov>
2020-05-14 14:49:09 -0500

    ch4/shm: move multi-module p2p logic into ipc

--- src/mpid/ch4/shm/ipc/src/ipc_impl.h
+++ src/mpid/ch4/shm/ipc/src/ipc_impl.h
@@ -3,9 +3,19 @@
  *     See COPYRIGHT in top-level directory
  */
 
-#ifndef XPMEM_IMPL_H_INCLUDED
-#define XPMEM_IMPL_H_INCLUDED
+#ifndef IPC_IMPL_H_INCLUDED
+#define IPC_IMPL_H_INCLUDED
 
 #include "mpidimpl.h"
 
-#endif /* XPMEM_IMPL_H_INCLUDED */
+#define MPIDI_IPC_PT2PT_DEFAULT 1
+#define MPIDI_IPC_PT2PT_MULTIMODS 2
+
+/* Enable multi-shmmods protocol when more than one shmmod is enabled. */
+#ifdef MPIDI_CH4_SHM_ENABLE_XPMEM
+#define MPIDI_IPC_PT2PT_PROT MPIDI_IPC_PT2PT_MULTIMODS
+#else
+#define MPIDI_IPC_PT2PT_PROT MPIDI_IPC_PT2PT_DEFAULT
+#endif
+
+#endif /* IPC_IMPL_H_INCLUDED */
